00de2e19378f413ca9a7d3162993021c11540352
Apoorv Sachan <apoorvsachan@fico.com>
2020-11-03 17:32:55 +0530

    controllers working

--- src/main/java/org/apoos/controller/SharedResourceController.java
+++ src/main/java/org/apoos/controller/SharedResourceController.java
@@ -1,12 +1,14 @@
 package org.apoos.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class SharedResourceController {
-    @GetMapping("/home")
-    public String login(){
-        return "login";
+    @GetMapping("/login")
+    public ModelAndView login(){
+        return new ModelAndView("login");
     }
 }
