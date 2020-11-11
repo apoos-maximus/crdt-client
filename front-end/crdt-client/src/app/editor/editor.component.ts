01e42709572d779fc26aadebf2ee9f2bd60a6938
Apoorv Sachan <apoorvsachan@fico.com>
2020-11-11 15:28:02 +0530

    newline working now

--- front-end/crdt-client/src/app/editor/editor.component.ts
+++ front-end/crdt-client/src/app/editor/editor.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editor.component.css']
 })
 export class EditorComponent implements OnInit {
-
+  contents : String;
+  
+  onKeyPress(evt: any) {
+    if (evt.key == "Enter") this.contents = this.contents + '<br/>';
+    else {
+      this.contents += evt.key;
+    }
+    
+    console.log(evt.key);
+  }
   constructor() { }
 
   ngOnInit(): void {
+    this.contents = "";
   }
 
 }
