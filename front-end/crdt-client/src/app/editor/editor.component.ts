2013bebf247ffe8bcf150c4b4edbf3850d78fab4
Apoorv Sachan <apoorvsachan@fico.com>
2020-11-11 22:07:49 +0530

    text area working

--- front-end/crdt-client/src/app/editor/editor.component.ts
+++ front-end/crdt-client/src/app/editor/editor.component.ts
@@ -7,36 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
   contents : String;
-  
-  onKeyPress(evt: any) {
- 
-    if( evt.keyCode !=32) {
-        if (evt.keyCode != 13){
-             this.contents += evt.key;
-        }
-        else {
-          this.contents += '<br/>';
-        }
-    } 
-    else {
-      this.contents += " ";
-      return false;
-    }
-  }
-
-  onKeyDown(evt :any) {
-    if (evt.keyCode == 8){
-      if (this.contents.length != 0)
-        this.contents = this.contents.slice(0,-1);
-    }
-  }
-
-
 
   constructor() { }
 
   ngOnInit(): void {
-    this.contents = "";
+    this.contents = "edit here !";
   }
 
 }
