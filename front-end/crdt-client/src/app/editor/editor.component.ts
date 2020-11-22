362348908bbdfb6f93def8e220b22d88e3a9a672
Apoorv Sachan <apoorvsachan@fico.com>
2020-11-23 03:14:17 +0530

    polling is working

--- front-end/crdt-client/src/app/editor/editor.component.ts
+++ front-end/crdt-client/src/app/editor/editor.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SyncService } from '../sync.service';
+import { Observable } from 'rxjs/Observable';
+import { Subscription, timer, Subject } from 'rxjs';
+import { switchMap, takeUntil, catchError, map, retry, share } from 'rxjs/operators';
+import { UserContent } from '../UserContent';
 
 @Component({
   selector: 'app-editor',
@@ -8,13 +12,27 @@ import { SyncService } from '../sync.service';
 })
 export class EditorComponent implements OnInit {
   contents : any;
+  subscription : Subscription;
+  userContent$ : Observable<UserContent>
+  private stopPolling = new Subject();
 
   constructor(private syncService : SyncService) { }
+  
 
   ngOnInit(): void {
-    this.syncService.getContents().subscribe((data) => {
-      this.contents = data.content;
-    })
+    this.userContent$ = timer(1,3000).pipe(
+      switchMap( ()=> this.syncService.getContents() ),
+      retry(),
+      share(),
+      takeUntil(this.stopPolling  )
+    )
+    this.subscription = this.userContent$.subscribe(
+      data => { this.contents = data.content}
+    )
   }
 
+  ngOnDestroy(){
+    this.stopPolling.next();
+    this.subscription.unsubscribe();
+  }
 }
